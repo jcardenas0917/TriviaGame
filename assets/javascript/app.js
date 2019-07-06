7430560c89363bf88d29e2e0df27a77defd1e52e
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-07-06 08:58:54 -0400

    started to clean up the code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,11 @@
 var intervalId;
 var currentQueIndex = 0;
 const initialTime = 3;
-const correctPage = "correct";
-const wrongPage = "wrong";
-const outOfTimePage = "time";
-const gameEndPage = "end";
 var totalCorrect = 0;
 var totalWrong = 0;
 var totalTimeOut = 0;
 var time = 0;
-const nextTime = 1000;
+const nextTime = 3000;
 var listOfQuestions = [
     {
         question: "What was the last song John Lennon played for a paying audience?",
@@ -134,52 +130,7 @@ var trivia = {
         console.log("list of questions length " + listOfQuestions.length);
        
     },
-    //show the page after each answer or time out
-    // switchToPage: function (page) {
-    //     console.log("inside pageswitch funciton");
-    //     if (page === correctPage) {
-    //         console.log("Switch to correctPage");
-    //         totalCorrect++;
-    //         $("#question").text("Correct!");
-    //         $("#question").css("font-size", "50px");
-    //         $(".answers").hide();
-    //         $(".image-container").show();
-    //         $(".image-container").html("<img src='assets/images/win.gif' width='300px'/>");
-    //         trivia.resetTime();
-
-    //     } else if (page === wrongPage) {
-    //         console.log("Switch to wrongePage");
-    //         totalWrong++;
-    //         $("#question").text("Wrong!");
-    //         $("#question").css("font-size", "50px");
-    //         $(".answers").hide();
-    //         $(".image-container").show();
-    //         $(".image-container").html("<img src='assets/images/losse.gif' width='300px'/>");
-    //         trivia.resetTime();
-    //     } else if (page === outOfTimePage) {
-    //         totalTimeOut++;
-    //         console.log("Switch to out of time page");
-    //         $("#question").text("Out Of Time!");
-    //         $("#question").css("font-size", "50px");
-    //         $(".answers").hide();
-    //         $(".image-container").show();
-    //         $(".image-container").html("<img src='assets/images/time.gif' width='300px'/>");
-    //         trivia.resetTime();
-    //     }
-
-    //     if (page === gameEndPage) {
-    //         console.log("Switch to game end page");
-    //         $("#question").text("All done, here is how you did");
-    //         $("#question").css("font-size", "50px");
-    //         $("#option1").text("Total correct: " + totalCorrect);
-    //         $("#option2").text("Total wrong: " + totalWrong);
-    //         $("#option3").text("Unanswered: " + totalTimeOut);
-    //         $("#option4").hide();
-    //         // $(".gameover").show();
-
-    //     }
-
-    // },
+   
     showCorrect: function () {
         console.log("Switch to correctPage");
         totalCorrect++;
