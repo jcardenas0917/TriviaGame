ec49ba6c774c6911b3a489305fc1f99b6094d852
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-07-05 23:19:36 -0400

    worked on js html and css done with most work 11:19pm 7/5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 
 var intervalId;
 var currentQueIndex = 0;
-const initialTime = 10;
+const initialTime = 3;
 const correctPage = "correct";
 const wrongPage = "wrong";
 const outOfTimePage = "time";
@@ -12,6 +12,7 @@ var totalCorrect = 0;
 var totalWrong = 0;
 var totalTimeOut = 0;
 var time = 0;
+const nextTime = 1000;
 var listOfQuestions = [
     {
         question: "What was the last song John Lennon played for a paying audience?",
@@ -69,15 +70,12 @@ var trivia = {
             trivia.stopClock();
             console.log("current index " + currentQueIndex);
             console.log("list of questions length " + listOfQuestions.length);
-            if (currentQueIndex >= listOfQuestions.length - 1) {
-                console.log("current index is greater than or equal to list of que")
-                trivia.switchToPage(gameEndPage);
-            } else {
-                trivia.switchToPage(outOfTimePage);
-                console.log("Increment current index from count()");
-                currentQueIndex++;
-            };
+
+            trivia.showOutOfTime();
+            console.log("Increment current index from count()");
+            // currentQueIndex++;
         };
+
 
     },
 
@@ -99,7 +97,7 @@ var trivia = {
     updateQuestion: function () {
 
         $("#question").css("font-size", "30px");
-        $(".answers").show();
+        $(".options").show();
         $(".image-container").hide();
         console.log("clock is running " + trivia.clockRunning);
         console.log("total correct " + totalCorrect);
@@ -120,81 +118,149 @@ var trivia = {
 
     //this function will check the answers
     checkAnswer: function (selected, answer) {
+
         if (selected === answer) {
 
             console.log("you win");
             trivia.stopClock();
-            trivia.switchToPage(correctPage);
+            trivia.showCorrect();
         } else {
             console.log("you losse");
             trivia.stopClock();
-            trivia.switchToPage(wrongPage);
+            trivia.showWrong();
         }
-        
+
         console.log("current index " + currentQueIndex);
         console.log("list of questions length " + listOfQuestions.length);
+       
+    },
+    //show the page after each answer or time out
+    // switchToPage: function (page) {
+    //     console.log("inside pageswitch funciton");
+    //     if (page === correctPage) {
+    //         console.log("Switch to correctPage");
+    //         totalCorrect++;
+    //         $("#question").text("Correct!");
+    //         $("#question").css("font-size", "50px");
+    //         $(".answers").hide();
+    //         $(".image-container").show();
+    //         $(".image-container").html("<img src='assets/images/win.gif' width='300px'/>");
+    //         trivia.resetTime();
+
+    //     } else if (page === wrongPage) {
+    //         console.log("Switch to wrongePage");
+    //         totalWrong++;
+    //         $("#question").text("Wrong!");
+    //         $("#question").css("font-size", "50px");
+    //         $(".answers").hide();
+    //         $(".image-container").show();
+    //         $(".image-container").html("<img src='assets/images/losse.gif' width='300px'/>");
+    //         trivia.resetTime();
+    //     } else if (page === outOfTimePage) {
+    //         totalTimeOut++;
+    //         console.log("Switch to out of time page");
+    //         $("#question").text("Out Of Time!");
+    //         $("#question").css("font-size", "50px");
+    //         $(".answers").hide();
+    //         $(".image-container").show();
+    //         $(".image-container").html("<img src='assets/images/time.gif' width='300px'/>");
+    //         trivia.resetTime();
+    //     }
+
+    //     if (page === gameEndPage) {
+    //         console.log("Switch to game end page");
+    //         $("#question").text("All done, here is how you did");
+    //         $("#question").css("font-size", "50px");
+    //         $("#option1").text("Total correct: " + totalCorrect);
+    //         $("#option2").text("Total wrong: " + totalWrong);
+    //         $("#option3").text("Unanswered: " + totalTimeOut);
+    //         $("#option4").hide();
+    //         // $(".gameover").show();
+
+    //     }
+
+    // },
+    showCorrect: function () {
+        console.log("Switch to correctPage");
+        totalCorrect++;
+        $("#question").text("Correct!");
+        $("#question").css("font-size", "50px");
+        $(".options").hide();
+        $(".image-container").show();
+        $(".image-container").html("<img src='assets/images/win.gif' width='300px'/>");
+        trivia.checkForNextPage();
+    },
+
+    showWrong: function () {
+        console.log("Switch to wrongePage");
+        totalWrong++;
+        $("#question").text("Wrong!");
+        $("#question").css("font-size", "50px");
+        $(".options").hide();
+        $(".image-container").show();
+        $(".image-container").html("<img src='assets/images/losse.gif' width='300px'/>");
+
+        trivia.checkForNextPage();
+
+    },
+
+    showOutOfTime: function () {
+        totalTimeOut++;
+        console.log("Switch to out of time page");
+        $("#question").text("Out Of Time!");
+        $("#question").css("font-size", "50px");
+        $(".options").hide();
+        $(".image-container").show();
+        $(".image-container").html("<img src='assets/images/time.gif' width='300px'/>");
+
+        trivia.checkForNextPage();
+
+    },
+
+    showEndOfGame: function () {
+        console.log("Switch to game end page");
+        $(".options").hide();
+        $(".results").show();
+        $("#question").text("All done, here is how you did");
+        $("#question").css("font-size", "50px");
+        $("#result1").text("Total correct: " + totalCorrect);
+        $("#result2").text("Total wrong: " + totalWrong);
+        $("#result3").text("Unanswered: " + totalTimeOut);
+        $("#result4").hide();
+        $(".image-container").hide();
+        $(".gameover").show();
+    },
+
+
+
+    //Checks for end of game or next page
+    checkForNextPage: function () {
+
         if (currentQueIndex >= listOfQuestions.length - 1) {
             console.log("current index is greater than or equal to list of que")
             trivia.stopClock();
-            trivia.switchToPage(gameEndPage);
+            setTimeout(function () {
+                trivia.showEndOfGame();
+            }, nextTime)
+
         } else {
             console.log("Increment current index from checkAnswer()");
             currentQueIndex++;
-        }
-    },
-    //show the page after each answer or time out
-    switchToPage: function (page) {
-        console.log("inside pageswitch funciton");
-        if (page === correctPage) {
-            console.log("Switch to correctPage");
-            totalCorrect++;
-            $("#question").text("Correct!");
-            $("#question").css("font-size", "50px");
-            $(".answers").hide();
-            $(".image-container").show();
-            $(".image-container").html("<img src='assets/images/win.gif' width='300px'/>");
-
-        } else if (page === wrongPage) {
-            console.log("Switch to wrongePage");
-            totalWrong++;
-            $("#question").text("Wrong!");
-            $("#question").css("font-size", "50px");
-            $(".answers").hide();
-            $(".image-container").show();
-            $(".image-container").html("<img src='assets/images/losse.gif' width='300px'/>");
-        } else if (page === outOfTimePage) {
-            totalTimeOut++;
-            console.log("Switch to out of time page");
-            $("#question").text("Out Of Time!");
-            $("#question").css("font-size", "50px");
-            $(".answers").hide();
-            $(".image-container").show();
-            $(".image-container").html("<img src='assets/images/time.gif' width='300px'/>");
-        }
-
-        if (page === gameEndPage) {
-            console.log("Switch to game end page");
-            $("#question").text("All done, here is how you did");
-            $("#question").css("font-size", "50px");
-            $("#option1").text("Total correct: " + totalCorrect);
-            $("#option2").text("Total wrong: " + totalWrong);
-            $("#option3").text("Unanswered: " + totalTimeOut);
-            $("#option4").hide();
-            // $(".gameover").show();
-
-        } else {
-            console.log("Call update question after 3 seconds");
             setTimeout(function () {
                 trivia.updateQuestion();
-            }, 3000)
+            }, nextTime)
+
         }
+
     },
+
     resetGame: function () { }
 };
 
 //Hides the play again button
 window.onload = function () {
     $(".gameover").hide();
+
 };
 
 $(".start").on("click", function () {
